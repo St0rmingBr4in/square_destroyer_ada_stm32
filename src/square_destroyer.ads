f8edb37850caa83af0d872f0c36adb8f5f37a0e8
Maxime Joubert <jouber_m@epita.fr>
2019-01-24 20:00:45 +0100

    contract: add more contracts
    * src/square_destroyer.ads: here.

--- src/square_destroyer.ads
+++ src/square_destroyer.ads
@@ -2,7 +2,8 @@ with HAL.Bitmap; use HAL.Bitmap;
 
 package Square_Destroyer is
 
-    procedure Square_Destroyer;
+    procedure Square_Destroyer with
+        Global => null;
 
 private
 
@@ -30,18 +31,22 @@ private
     function Is_In_Grid(P : Point) return Boolean with
         SPARK_MODE => On,
         Global     => null,
+        Depends    => (Is_In_Grid'Result => P),
         Post       => (Is_In_Grid'Result = (P.X in 1..GRID_WIDTH and then
                                             P.Y in 1..GRID_HEIGHT));
 
     procedure Init_Grid(G : out Grid) with
-        Global => null,
-        Post   => (for all I in G'Range(1) =>
-                       (for all J in G'Range(2) =>
-                            G(I, J)'Valid));
+        Global  => null,
+        Depends => (G => null),
+        Post    => (for all I in G'Range(1) =>
+                        (for all J in G'Range(2) =>
+                             G(I, J)'Valid));
 
-    procedure Init_Board;
+    procedure Init_Board with
+        Global => null;
 
-    procedure Draw_Grid(G : Grid);
+    procedure Draw_Grid(G : Grid) with
+        Global => null;
 
     procedure Swap(G : in out Grid; A : Point; B : Point) with
         SPARK_MODE => On,
@@ -55,6 +60,7 @@ private
     return Boolean with
         SPARK_MODE => On,
         Global     => null,
+        Depends    => (Are_Adjacent'Result => (A, B)),
         Pre        => ((not A.Valid or else Is_In_Grid(A.P)) and then
                        (not B.Valid or else Is_In_Grid(B.P))),
         Post       => (if not A.Valid or else not B.Valid then
@@ -66,7 +72,10 @@ private
 
     function Count_Dir(G : Grid; X : Integer; Step_X : Integer; Y : Integer;
                        Step_Y : Integer; S : Square)
-      return Integer;
-    function Is_Move_Legal(G : Grid; P : Point) return Boolean;
+    return Integer with
+        Global => null;
+
+    function Is_Move_Legal(G : Grid; P : Point) return Boolean with
+        Global => null;
 
 end Square_Destroyer;
