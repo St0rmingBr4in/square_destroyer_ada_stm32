17dd550323499bb7ee83f8b6a042fa1e6940d17c
Maxime Joubert <jouber_m@epita.fr>
2019-01-24 01:26:54 +0100

    code: clean: remove adacore copyright
    * src/square_destroyer.adb: here.

--- src/square_destroyer.adb
+++ src/square_destroyer.adb
@@ -1,34 +1,3 @@
-------------------------------------------------------------------------------
---                                                                          --
---                     Copyright (C) 2015-2016, AdaCore                     --
---                                                                          --
---  Redistribution and use in source and binary forms, with or without      --
---  modification, are permitted provided that the following conditions are  --
---  met:                                                                    --
---     1. Redistributions of source code must retain the above copyright    --
---        notice, this list of conditions and the following disclaimer.     --
---     2. Redistributions in binary form must reproduce the above copyright --
---        notice, this list of conditions and the following disclaimer in   --
---        the documentation and/or other materials provided with the        --
---        distribution.                                                     --
---     3. Neither the name of the copyright holder nor the names of its     --
---        contributors may be used to endorse or promote products derived   --
---        from this software without specific prior written permission.     --
---                                                                          --
---   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
---   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
---   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
---   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
---   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
---   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
---   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
---   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
---   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
---   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
---   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
---                                                                          --
-------------------------------------------------------------------------------
-
 with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
 --  The "last chance handler" is the user-defined routine that is called when
 --  an exception is propagated. We need it in the executable, therefore it
@@ -60,9 +29,9 @@ package body Square_Destroyer is
     procedure Draw_Grid(g : Grid) is
         type ColorMap is array(Square) of HAL.Bitmap.Bitmap_Color;
 
-        m : constant ColorMap := (HAL.Bitmap.Blue, HAL.Bitmap.Green, HAL.Bitmap.Red,
-        HAL.Bitmap.Yellow, HAL.Bitmap.Magenta,
-        HAL.Bitmap.Cyan);
+        m : constant ColorMap := (HAL.Bitmap.Blue, HAL.Bitmap.Green,
+                                  HAL.Bitmap.Red, HAL.Bitmap.Yellow,
+                                  HAL.Bitmap.Magenta, HAL.Bitmap.Cyan);
         Rect_Pos : Point := (0, 0);
         r        : Rect  := (Rect_Pos, 39, 39);
     begin
