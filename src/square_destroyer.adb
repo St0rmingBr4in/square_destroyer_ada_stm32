b60e7ce1512ee25c7c9016d5203ee506ef3dd6b9
Maxime Joubert <jouber_m@epita.fr>
2019-01-26 22:50:05 +0100

    Update_Grid: Remove one level of if imbrication
    * src/square_destroyer.adb: here.

--- src/square_destroyer.adb
+++ src/square_destroyer.adb
@@ -110,49 +110,53 @@ package body Square_Destroyer is
         WorkListMove        : PointSet.Set;
         WorkListCombination : PointSet.Set;
     begin
-        if Are_Adjacent(Cur_Square, Last_Square) then
-            Swap(G, Cur_Square.P, Last_Square.P);
-            -- we want to evaluate both to fill our worklist
-            Is_Move_Legal(G, Cur_Square.P, WorkListMove);
-            Is_Move_Legal(G, Last_Square.P, WorkListMove);
-            if not PointSet.Is_Empty(WorkListMove) then
-                Last_Square.Valid := False;
-                Cur_Square.Valid  := False;
-                Just_Moved        := True;
-            else
-                Swap(G, Cur_Square.P, Last_Square.P);
-                return;
-            end if;
-            while not PointSet.Is_Empty(WorkListMove) loop
-                while not PointSet.Is_Empty(WorkListMove) loop
-                    declare
-                        Element : constant Point :=
-                            PointSet.First_Element(WorkListMove);
-                        Tmp_Y : Integer := Element.Y;
-                    begin
-                        while Tmp_Y > 0 loop
-                            exit when not PointSet.Contains(WorkListMove,
-                                                           (Element.X, Tmp_Y));
-                            Tmp_Y := Tmp_Y - 1;
-                        end loop;
-                        if Tmp_Y = 0 then
-                            g(Element.X, Element.y) := Get_Random_Square;
-                        else
-                            g(Element.X, Element.Y) := g(Element.X, Tmp_Y);
-                            PointSet.Include(WorkListMove, (Element.X, Tmp_Y));
-                        end if;
-                        PointSet.Delete_First(WorkListMove);
-                        PointSet.Include(WorkListCombination, Element);
-                    end;
-                end loop;
-                while not PointSet.Is_empty(WorkListCombination) loop
-                    Is_Move_Legal(G,
-                                  PointSet.First_Element(WorkListCombination),
-                                  WorkListMove);
-                    PointSet.Delete_First(WorkListCombination);
-                end loop;
-            end loop;
+        if not Are_Adjacent(Cur_Square, Last_Square) then
+            Just_Moved := False;
+            return;
         end if;
+
+        Swap(G, Cur_Square.P, Last_Square.P);
+        -- we want to evaluate both to fill our worklist
+        Is_Move_Legal(G, Cur_Square.P, WorkListMove);
+        Is_Move_Legal(G, Last_Square.P, WorkListMove);
+        if not PointSet.Is_Empty(WorkListMove) then
+            Last_Square.Valid := False;
+            Cur_Square.Valid  := False;
+            Just_Moved        := True;
+        else
+            Swap(G, Cur_Square.P, Last_Square.P);
+            Just_Moved := False;
+            return;
+        end if;
+
+        while not PointSet.Is_Empty(WorkListMove) loop
+            while not PointSet.Is_Empty(WorkListMove) loop
+                declare
+                    Element : constant Point :=
+                        PointSet.First_Element(WorkListMove);
+                    Tmp_Y : Integer := Element.Y;
+                begin
+                    while Tmp_Y > 0 loop
+                        exit when not PointSet.Contains(WorkListMove,
+                                                       (Element.X, Tmp_Y));
+                        Tmp_Y := Tmp_Y - 1;
+                    end loop;
+                    if Tmp_Y = 0 then
+                        g(Element.X, Element.y) := Get_Random_Square;
+                    else
+                        g(Element.X, Element.Y) := g(Element.X, Tmp_Y);
+                        PointSet.Include(WorkListMove, (Element.X, Tmp_Y));
+                    end if;
+                    PointSet.Delete_First(WorkListMove);
+                    PointSet.Include(WorkListCombination, Element);
+                end;
+            end loop;
+            while not PointSet.Is_empty(WorkListCombination) loop
+                Is_Move_Legal(G, PointSet.First_Element(WorkListCombination),
+                              WorkListMove);
+                PointSet.Delete_First(WorkListCombination);
+            end loop;
+        end loop;
     end Update_Grid;
 
     procedure Swap(G : in out Grid; A : Point; B : Point) is
@@ -203,14 +207,14 @@ package body Square_Destroyer is
     end Sort_By_Height;
 
     procedure Is_Move_Legal(G : Grid; P : Point; Combinations : in out
-        PointSet.Set) is
-        S : constant Square := G(P.X, P.Y);
+                            PointSet.Set) is
+        S                   : constant Square := G(P.X, P.Y);
         Matching_Horizontal : PointSet.Set;
-        Matching_Vertical : PointSet.Set;
-        Legal : Boolean := False;
+        Matching_Vertical   : PointSet.Set;
+        Legal               : Boolean := False;
     begin
         Get_Matching_Neighbourgs(G, P.X - 1, -1, P.Y, 0, S,
-        Matching_Horizontal);
+                                 Matching_Horizontal);
         Get_Matching_Neighbourgs(G, P.X + 1, 1, P.Y, 0, S, Matching_Horizontal);
         Get_Matching_Neighbourgs(G, P.X, 0, P.Y - 1, -1, S, Matching_Vertical);
         Get_Matching_Neighbourgs(G, P.X, 0, P.Y + 1, 1, S, Matching_Vertical);
